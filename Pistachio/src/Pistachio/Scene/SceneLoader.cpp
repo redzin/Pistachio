c82146efe09c0bfb7afb71f0e1acfc3a2a23207b
Jessica <Hansen>
2022-10-23 15:14:54 +0200

    Added tinygltf submodule

--- Pistachio/src/Pistachio/Scene/SceneLoader.cpp
+++ Pistachio/src/Pistachio/Scene/SceneLoader.cpp
@@ -40,7 +40,7 @@ namespace glm
 
     for (int i = 0; i < 4; i++)
       for (int j = 0; j < 4; j++)
-        mat[i][j] = jsonArray[i * 4 + j];
+        mat[i][j] = jsonArray[i + j * 4];
     
   }
 }
